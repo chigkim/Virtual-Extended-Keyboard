abfb4f3b40bf752c35c284cd37e7008070217244
chigkim <chigkim@github.com>
2018-11-12 12:13:05 -0500

    Initial Commit

---
+++ Virtual Extended Keyboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Virtual Extended Keyboard
+//
+//  Created by Chi Kim on 11/12/18.
+//  Copyright © 2018 Chi Kim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Virtual Extended Keyboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Virtual Extended Keyboard
+//
+//  Created by Chi Kim on 11/12/18.
+//  Copyright © 2018 Chi Kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
