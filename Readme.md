b661375a266fedc62cfa6edabdbb17c7f82e5c5d
chigkim <chigkim@github.com>
2019-10-27 08:57:58 -0400

    v0.1.0

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Virtual Extended Keyboard
+Virtual Extended Keyboard allows you to type numpad using Mac keyboard without numpad. It also allows you to type f1-f20 on Macbook with touch bar without using touch bar.
+
+Virtual Extended Keyboard is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY, expressed or implied, of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Please see the [GNU General Public License](http://www.gnu.org/licenses/) for more details.
+
+## Download
+Here is the direct link to download [Virtual Extended Keyboard v0.1.0.](https://github.com/chigkim/vek/releases/download/v0.1.0/VOCR.v0.1.0-alpha.10.zip)  
+
+## Installation
+* Copy the Virtual Extended Keyboard.app to your Applications folder.
+* First time when you run, it'll display a system alert asking you to allow accessibility permission in system preference, and the app will quit.
+* Grant the accessibility permission in system preference and run the app again
+* Make sure you can find the app in menu extra by pressing vo+m twice
+* Open up a text editor and press fn+j, fn+k, fn+l, and it should type 1 2 3
+
+## Mapping
+* Num 1 2 3: fn j k l
+* Num 4 5 6: fn u i o
+* Num 7 8 9: fn 7 8 9
+* Num 0: fn m
+* Num .: fn .
+* Num *: fn ,
+* Num /: fn /
+* Num =: fn ;
+* Num -: fn -
+* Num +: fn =
+* f1 f2 f3 f4: fn 1 2 3 4
+* f5 f6 f7 f8: fn-q w e r
+* f9 f10 f11 f12: fn a s d f
+* f16 f17 f18 f19: fn z x c v
--- Virtual Extended Keyboard/AppDelegate.swift
+++ Virtual Extended Keyboard/AppDelegate.swift
@@ -10,17 +10,83 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+	
+	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+	
+	func checkAccess(ask:Bool) -> Bool {
+		let prompt = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString
+		let options = [prompt: ask]
+		return AXIsProcessTrustedWithOptions(options as CFDictionary?)
+	}
+	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		if !checkAccess(ask:true) {
+			NSApplication.shared.terminate(self)
+		}
+		
+		let fileManager = FileManager.default
+		let home = fileManager.homeDirectoryForCurrentUser
+		let launchFolder = home.appendingPathComponent("Library/LaunchAgents")
+		if !fileManager.fileExists(atPath: launchFolder.path) {
+			try! fileManager.createDirectory(at: launchFolder, withIntermediateDirectories: false, attributes: nil)
+		}
+		let launchPath = "Library/LaunchAgents/com.chikim.Virtual-Extended-Keyboard.plist"
+		let launchFile = home.appendingPathComponent(launchPath)
+		let defaults = UserDefaults.standard
+		if !defaults.bool(forKey: "launchedBefore") && !fileManager.fileExists(atPath: launchFile.path) {
+			let bundle = Bundle.main
+			let bundlePath = bundle.path(forResource: "com.chikim.Virtual-Extended-Keyboard", ofType: "plist")
+			try! fileManager.copyItem(at: URL(fileURLWithPath: bundlePath!), to: launchFile)
+			defaults.set(true, forKey: "launchedBefore")
+		}
+		
+		let menu = NSMenu()
+		let menuItem = NSMenuItem(title: "Launch on Login", action: #selector(AppDelegate.toggleLaunch(_:)), keyEquivalent: "")
+		if fileManager.fileExists(atPath: launchFile.path) {
+			menuItem.state = .on
+		}
+		
+		menu.addItem(menuItem)
+		menu.addItem(withTitle: "Quit", action: #selector(AppDelegate.quit(_:)), keyEquivalent: "")
+		statusItem.menu = menu
+		
+		if let button = statusItem.button {
+			button.image = NSImage(named: "Virtual-Extended-Keyboard-StatusBar-Icon")
+			button.action = #selector(AppDelegate.click(_:))
+		}
+		NSApplication.shared.windows[1].close()
+		NSApplication.shared.hide(self)
+		NSApplication.shared.deactivate()
 	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
+	
+	@objc func click(_ sender: Any?) {
+		debugPrint("ok")
 	}
-
-
+	
+	@objc func quit(_ sender: AnyObject?) {
+		NSApplication.shared.terminate(self)
+	}
+	
+	@objc func toggleLaunch(_ sender: AnyObject?) {
+		let fileManager = FileManager.default
+		let home = fileManager.homeDirectoryForCurrentUser
+		let launchPath = "Library/LaunchAgents/com.chikim.Virtual-Extended-Keyboard.plist"
+		let launchFile = home.appendingPathComponent(launchPath)
+		let menu = statusItem.menu
+		let menuItem = menu?.item(withTitle:"Launch on Login")
+		if menuItem?.state == .off {
+			if !fileManager.fileExists(atPath: launchFile.path) {
+				let bundle = Bundle.main
+				let bundlePath = bundle.path(forResource: "com.chikim.Virtual-Extended-Keyboard", ofType: "plist")
+				try! fileManager.copyItem(at: URL(fileURLWithPath: bundlePath!), to: launchFile)
+			}
+			menuItem?.state = .on
+		} else {
+			try!fileManager.removeItem(at: launchFile)
+			menuItem?.state = .off
+		}
+	}
+	
+	
 }
 
